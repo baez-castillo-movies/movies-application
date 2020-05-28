062da9bf05d8e1202fce5f3470b583fe9e071a10
JahzielB <jahzielb@gmail.com>
2020-05-28 15:10:37 -0500

    working on editing existing movies

--- src/index.js
+++ src/index.js
@@ -8,20 +8,25 @@
  * require style imports
  */
 // const {getMovies} = require('./api.js');
-import { getMovies } from './api';
-// import {formClick} from "./form";
+import {getMovies} from './api';
 
-console.log(getMovies);
-
+// creates a list after the promise returns
 let makeMovies = () => {
   getMovies().then((movies) => {
     // console.log('Here are all the movies:');
     document.querySelector(".container").innerHTML = "<ul id='movies'></ul>"
     movies.forEach(({title, rating, id}) => {
+      // creates and shows the current movie db
       let list = document.createElement('li');
       let currentMovie = document.createTextNode(`ID:${id} Title:${title} Rating:${rating}`);
       list.appendChild(currentMovie);
       document.querySelector('#movies').appendChild(list);
+
+      // populates the dropdown with the current movies in the db
+      let dropdown = document.createElement("option");
+      let currentDropdown = document.createTextNode(`${title}`);
+      dropdown.appendChild(currentDropdown);
+      document.querySelector("#current-movie").appendChild(dropdown);
     });
   }).catch((error) => {
     alert('Oh no! Something went wrong.\nCheck the console for details.')
@@ -31,6 +36,7 @@ let makeMovies = () => {
 
 makeMovies();
 
+// adds movies to the db.json
 document.querySelector('#movieBtn').addEventListener("click", function (e) {
   e.preventDefault();
   let mTitle = document.querySelector('#title').value;
@@ -51,5 +57,7 @@ document.querySelector('#movieBtn').addEventListener("click", function (e) {
 
 
 
-
-
+document.querySelector('#current-movie').addEventListener("click", function () {
+  let dropdownValue = document.querySelector('#current-movie').value;
+  console.log(dropdownValue);
+});
